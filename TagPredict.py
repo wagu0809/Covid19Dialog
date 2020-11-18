ea39680bdb0163f9f41003c6efe0b4c118f9639f
wagu0809 <wagu0809@sina.com>
2020-11-18 11:49:19 +0800

    Update TagPredict.py
    防止预测结果为空时产生的空指针错误

--- TagPredict.py
+++ TagPredict.py
@@ -48,16 +48,17 @@ class TagPredict:
 
         result = recover_result(self.dataloader, tag_logits[0][0], tag_mask_tensor[0], word_squence, new2ori)
 
-        return result, self.label2id[result[-1]]
+        print(f"result: {result}")
+        return result, self.label2id[result[-1]] if result[-1] != '' else 0
 
 
 if __name__ == '__main__':
     config_dir = './configs/configs.json'
-    model_dir = './output/pytorch_model.bin'
+    model_dir = './output/pytorch_model_covid.bin'
     tag_dir = './data/covid/tag_vocab.json'
     label_dir = './data/covid/label_vocab.json'
     predictor = TagPredict(config_dir, model_dir, tag_dir, label_dir)
-    keywords = json.loads(open('./data/keywords.json', encoding='utf-8').read())
+    keywords = json.loads(open('./data/covid/keywords_dict.json', encoding='utf-8').read())
     # question = '出现皮肤发红和发炎的症状可能与什么疾病有关？'  # (['发炎'], '涉及症状')
     # question = '哪种病会与咽部溃疡以及鼻衄症状有关？'  # (['咽部溃疡', '鼻衄'], '涉及症状')
     # question = '从医学专科上来讲，结核病属于什么科？'  # (['结核病'], '医学专科;就诊;就诊科室')
@@ -73,7 +74,9 @@ if __name__ == '__main__':
     # question = '删除用户的常见命令是什么?'  # (['删除用户'], '常见命令')
     # question = '数据库的定义是什么?'  # (['数据库'], '定义')
     # question = '用户授权的概念是什么?'  # (['用户授权'], '概念')
-    question = '举个关系型数据库的例子.'  # (['关系型数据库'], '例子')
+    # question = '举个关系型数据库的例子.'  # (['关系型数据库'], '例子')
+    # question = '我经常耳闷，应该去哪个科室进行检查？'  # (['关系型数据库'], '例子')
+    question = '在医学专科领域中，细菌性肺炎属于什么疾病？'  # (['关系型数据库'], '例子')
     result, label_id = predictor.predict(question)
 
     print(result, label_id)
